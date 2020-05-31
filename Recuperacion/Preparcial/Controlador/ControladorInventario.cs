33e925d889aaca9de6a99bb73ce51537ad0b43d4
Cj72001 <52006093+Cj72001@users.noreply.github.com>
2020-05-31 14:25:14 -0600

    Recuperacion_Parcial_02

---
+++ Recuperacion/Preparcial/Controlador/ControladorInventario.cs
@@ -0,0 +1,113 @@
+﻿using Preparcial.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Preparcial.Controlador
+{
+    public static class ControladorInventario
+    {
+        
+        public static DataTable GetProductosTable()
+        {
+            DataTable productos = null;
+
+            
+            try
+            {
+                productos = ConexionBD.EjecutarConsulta("SELECT * FROM INVENTARIO");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return productos;
+        }
+
+        
+        public static List<Inventario> GetProductos()
+        {
+            var productos = new List<Inventario>();
+            DataTable tableArticulos = null;
+            
+            //Correcion: Poner Try catch solamente en la consulta
+            try
+            {
+                tableArticulos = ConexionBD.EjecutarConsulta("SELECT * FROM INVENTARIO");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+            
+            foreach(DataRow dr in tableArticulos.Rows)
+            {
+                productos.Add(new Inventario
+                    (
+                    
+                        dr[0].ToString(),
+                        dr[1].ToString(),
+                        dr[2].ToString(),
+                        dr[3].ToString(),
+                        dr[4].ToString()
+                    )
+                );
+            }
+
+            return productos;
+            
+        }
+
+              
+                
+
+        
+        public static void AnadirProducto(string nombre, string descripcion, string precio, string stock)
+        {
+            try
+            {
+                
+                ConexionBD.EjecutarComando("INSERT INTO INVENTARIO(nombreArt, descripcion, precio, stock)" +
+                    $" VALUES('{nombre}', '{descripcion}', {precio}, {stock})");
+
+                MessageBox.Show("Se ha agregado el producto");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+
+        
+        public static void EliminarProducto(string id)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando($"DELETE FROM INVENTARIO WHERE idArticulo = {id}");
+
+                MessageBox.Show("Se ha eliminado el producto");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+
+        
+        public static void ActualizarProducto(string id, string stock)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando($"UPDATE INVENTARIO SET stock = {stock} WHERE idArticulo = {id}");
+
+                MessageBox.Show("Se ha actualizado el producto");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+    }
+}
---
+++ Recuperacion/Preparcial/Vista/FrmLogin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Preparcial.Controlador;
+using Preparcial.Modelo;
+using Preparcial.Vista;
+
+namespace Preparcial
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile("../../Recursos/UCA.png");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+            PoblarControlers();
+        }
+        
+        
+        
+        private void PoblarControlers()
+        {
+            cmbUser.DataSource = null;
+            //Correcion: En el objeto "Usuario" el get es "Contrasena" no Contrasenia
+             cmbUser.ValueMember = "Contrasena";
+             cmbUser.DisplayMember = "NombreUsuario";
+             cmbUser.DataSource = ControladorUsuario.GetUsuarios();
+        }
+        
+
+        private void BttnLogin_Click(object sender, EventArgs e)
+        {
+            // var obtenerUsuario = (Usuario)cmbUser.SelectedItem;
+            //
+            // PoblarControlers();
+
+            if (textBox1.Text.Equals(cmbUser.SelectedValue.ToString()))
+            {
+                
+                FrmMain frmMain = new FrmMain((Usuario)cmbUser.SelectedItem);
+                Hide();
+                frmMain.Show();
+            }
+            else
+                MessageBox.Show("Contrasena incorrecta");
+        }
+
+        
+        private void BttnUpdatePassword_Click(object sender, EventArgs e)
+        {
+            FrmPassword frmPassword = new FrmPassword();
+            frmPassword.Show();
+            
+            //Correcion: Volver a poblar los controles por si se cambio la contrasena antes de ingresar
+            // PoblarControlers();
+        }
+    }
+}
